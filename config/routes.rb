fb1e652ce0d863c2e41a0e1f3c6f6953db3f5327
Philip Samoei <samphilkerich04@gmail.com>
2023-09-17 22:13:39 +0300

    add client and auth

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+ post "/login",to: "sessions#create"
+ get "/me", to: "users#show"
+ delete "/logout", to: "sessions#destroy"
 end
