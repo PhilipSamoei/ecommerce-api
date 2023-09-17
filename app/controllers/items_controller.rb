e4dd877497b0392a7add0761ed4325142e8451de
Philip Samoei <samphilkerich04@gmail.com>
2023-09-17 19:41:12 +0300

    faker for seed data

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,34 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items, status: :ok
+    end
+    def show
+        item = find_item
+        render json: item, status: :ok
+    end
+    def create
+        item  = Item.create!(item_params)
+        render json: item, status: :created
+
+    end
+    def update
+        item = Item.find(params[:id])
+        item.update(item_params)
+            render json: item, status: :ok
+    end
+
+    def destroy
+        item = Item.find
+        item.destroy
+        head :no_content
+    end
+    private
+    def find_item
+        Item.find(params[:id])
+    end
+    def item_params
+        params.permit(:name, :description, :price, :url, :saler)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Create some fake items
+10.times do
+  Item.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 10..1000, as_string: true),
+    description: Faker::Lorem.paragraph,
+    saler: Faker::Name.name,
+    url: Faker::Internet.url
+  )
+end
+
+puts "Seed data created successfully."
